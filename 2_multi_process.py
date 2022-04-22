482e19d0af421535ef8271a7210518b8773bf1d5
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2022-04-23 00:09:44 +0800

    Extract functions to components.py

--- 2_multi_process.py
+++ 2_multi_process.py
@@ -1,36 +1,8 @@
-import sys
-import time
 from threading import Thread
 from multiprocessing import Process, Queue
 
 from components import *
 
-
-def mul_with_time(q1: Queue, q2: Queue) -> int:
-    round = 0
-    while True and round < 2:
-        n = q1.get()
-        start = time.time()
-
-        q2.put(mul(n))
-        round += 1
-
-        end = time.time()
-        sys.stderr.write(f'Time cost for mul {end - start}\n')
-        sys.stderr.flush()
-
-def add_with_time(q: Queue) -> int:
-    round = 0
-    while True and round < 2:
-        n = q.get()
-        start = time.time()
-
-        add(n)
-        round += 1
-
-        end = time.time()
-        sys.stderr.write(f'Time cost for mul {end - start}\n')
-        sys.stderr.flush()
 
 if __name__ == '__main__':
     q1 = Queue()
--- components.py
+++ components.py
@@ -1,3 +1,5 @@
+import sys
+import time
 
 def add(n: int) -> int:
     result = 0
@@ -17,4 +19,32 @@ def mul(n: int) -> None:
             result *= (i + 1)
 
     return result
+
+
+def mul_with_time(q1, q2) -> int:
+    round = 0
+    while True and round < 2:
+        n = q1.get()
+        start = time.time()
+
+        q2.put(mul(n))
+        round += 1
+
+        end = time.time()
+        sys.stderr.write(f'Time cost for mul {end - start}\n')
+        sys.stderr.flush()
+
+
+def add_with_time(q) -> int:
+    round = 0
+    while True and round < 2:
+        n = q.get()
+        start = time.time()
+
+        add(n)
+        round += 1
+
+        end = time.time()
+        sys.stderr.write(f'Time cost for mul {end - start}\n')
+        sys.stderr.flush()
 
